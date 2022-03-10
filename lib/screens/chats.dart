83ade290d696f14e33b71d069ee8010a85e54dc4
OnelLiyanage <onel111liyanage@gmail.com>
2022-03-10 22:56:27 +0530

    chat-ui-basic-homescreen

---
+++ lib/screens/chats.dart
@@ -0,0 +1,82 @@
+import 'package:chat_ui/helper/authenticate.dart';
+import 'package:chat_ui/screens/chatinterface.dart';
+import 'package:chat_ui/services/authentication.dart';
+import 'package:flutter/material.dart';
+
+class ChatRoom extends StatefulWidget {
+	@override
+	_ChatRoomState createState() => _ChatRoomState();
+}
+
+class _ChatRoomState extends State<ChatRoom> {
+
+	// AuthMethods authMethods = new AuthMethods();
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar (
+				title: Image.asset("assets/images/logo.png",
+				height: 50,
+				),
+				actions: [
+					GestureDetector(
+						onTap: () {
+							// authMethods.signOut();
+							Navigator.pushReplacement(context, MaterialPageRoute(
+							builder: (context) => Authenticate() 
+							));
+						},
+						child: Container (
+							padding: EdgeInsets.symmetric(horizontal: 20.0),
+							child: Icon(Icons.logout),
+						),
+					),
+				],
+			),
+			body: Column(
+				mainAxisSize: MainAxisSize.max,
+				mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+				crossAxisAlignment: CrossAxisAlignment.center,
+				children: [
+					GestureDetector(
+										onTap: () {
+											Navigator.push(context, MaterialPageRoute(
+				   							builder: (context) => MyHomePage()
+											));
+										},
+										  child: Center(
+											  child: Text(
+												  "Scan for Melanoma",
+												  style : TextStyle (
+													  fontSize: 18.0,
+													  color : Colors.black,
+												  ),
+											  ),
+										  ),
+										),
+					Text(
+						"Option 2",
+						style : TextStyle (
+							fontSize: 18.0,
+							color : Colors.black,
+						),
+					),
+					Text(
+						"Option 3",
+						style : TextStyle (
+							fontSize: 18.0,
+							color : Colors.black,
+						),
+					),Text(
+						"Option 4",
+						style : TextStyle (
+							fontSize: 18.0,
+							color : Colors.black,
+						),
+					)
+				],
+			),
+		);
+	}
+}
---
+++ lib/screens/signin.dart
@@ -0,0 +1,103 @@
+import 'package:chat_ui/widgets/widget.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+
+	final	Function toggle;
+	SignIn(this.toggle);
+	
+	@override
+	_SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold (
+			appBar: AppBar (
+				title: Image.asset("assets/images/logo.png",
+				height: 50,
+				),
+			),
+
+			body: Container(
+				alignment: Alignment.bottomCenter,
+							child: Container(
+								padding: EdgeInsets.symmetric(horizontal: 25.0),
+									child: Column (
+										mainAxisSize: MainAxisSize.min,
+									children: [
+										TextField(
+											decoration: inputFeildDecoration ("Email"),
+										),
+									SizedBox(height: 20.0),
+
+										TextField(
+											decoration: inputFeildDecoration ("Password"),
+										),
+									SizedBox(height: 20.0),
+
+										Container(
+											child: Container ( 
+												alignment: Alignment.centerRight,
+												padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+												child: Text (
+													"Forgot Password",
+													style: TextStyle (
+														fontSize: 14.0
+													),
+												),
+											),
+										),
+										SizedBox( height: 15.0,),
+										Container(
+											alignment: Alignment.center,
+											width: MediaQuery.of(context).size.width,
+											padding: EdgeInsets.symmetric(vertical: 20),
+											decoration: BoxDecoration(
+												gradient: LinearGradient (
+													colors:  [
+														const Color(0xff007EF4),
+														const Color(0xff2A75DA) 
+														]
+												),
+												borderRadius: BorderRadius.circular(25)
+											),
+											child: Text (
+												"Login",
+												style: TextStyle (
+													fontSize: 18.0,
+													color: Colors.white,
+												),
+											),
+										),
+										SizedBox(height: 20.0),
+										Row(
+											mainAxisAlignment: MainAxisAlignment.center,
+											children: [
+												Text ("Dont have an account? "),
+												GestureDetector (
+													onTap: () {
+														widget.toggle();
+													},
+													child: Container(
+														padding: EdgeInsets.symmetric(vertical: 10.0),
+													child: Text(
+														" Sign Up here ",
+														style: TextStyle (
+															decoration: TextDecoration.underline
+														),
+														),
+													),
+												)
+											],
+										),
+										SizedBox(height: 100.0),
+									],
+								),
+							),
+			),
+		);
+	}
+}
